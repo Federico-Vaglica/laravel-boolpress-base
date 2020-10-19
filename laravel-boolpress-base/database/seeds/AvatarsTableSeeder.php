1b64293415e05d3e63689487ef04a3d8b25643b1
federicov <vaglicafederico95@gmail.com>
2020-10-19 17:23:11 +0200

    Avatar Seeder Done

--- laravel-boolpress-base/database/seeds/AvatarsTableSeeder.php
+++ laravel-boolpress-base/database/seeds/AvatarsTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Avatar;
+use App\User;
 
 class AvatarsTableSeeder extends Seeder
 {
@@ -13,6 +14,15 @@ class AvatarsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        
+        $users = User::all();
+
+        foreach($users as $user){
+            $avatarNew = new Avatar();
+            $avatarNew->telefono = $faker->phoneNumber;
+            $avatarNew->avatar = $faker->imageUrl(640,480);
+            $avatarNew->user_id = $user->id;
+            $avatarNew->save();
+        }
+
     }
 }
